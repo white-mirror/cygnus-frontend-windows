c2678f3accdb2455b1212d194986b5a1b0d008cf
Nicolas Panaino <npanaino@gmail.com>
2025-10-24 18:40:11 +0000

    feat: seed frontend windows module

---
+++ main.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+const getRendererEntry = () => {
+  if (app.isPackaged) {
+    return path.join(process.resourcesPath, "app", "dist", "index.html");
+  }
+  return path.resolve(__dirname, "../frontend-web/dist/index.html");
+};
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+    },
+  });
+
+  const entryFile = getRendererEntry();
+  window.loadFile(entryFile);
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
