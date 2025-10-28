01f9144269766aa3c332545c41e5fbcabf1bb83d
Nicolas Panaino <npanaino@gmail.com>
2025-10-28 23:16:06 +0000

    feat: expose desktop api config

--- preload.js
+++ preload.js
@@ -1,3 +1,12 @@
 const { contextBridge } = require("electron");
 
-contextBridge.exposeInMainWorld("cygnusDesktop", {});
+const DEFAULT_API_BASE_URL =
+  process.env.CYGNUS_API_BASE_URL ??
+  process.env.VITE_API_BASE_URL ??
+  "http://localhost:4000";
+
+contextBridge.exposeInMainWorld("cygnusDesktop", {
+  config: {
+    apiBaseUrl: DEFAULT_API_BASE_URL,
+  },
+});
